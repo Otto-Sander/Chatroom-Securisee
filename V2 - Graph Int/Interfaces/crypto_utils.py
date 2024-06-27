8beccc60db6a8a90f0ae417ac6cd64b30e06ed80
Arthur-Cstln <104087861+Arthur-Cstln@users.noreply.github.com>
2024-06-27 16:00:13 +0200

    encryption OK !

--- V2 - Graph Int/Interfaces/crypto_utils.py
+++ V2 - Graph Int/Interfaces/crypto_utils.py
@@ -13,11 +13,25 @@ def aes_encrypt(message, key):
 
 def aes_decrypt(encrypted_message, key):
     """ Decrypt a message using AES encryption with EAX mode. """
-    encrypted_message = base64.b64decode(encrypted_message)
-    nonce, tag, ciphertext = encrypted_message[:16], encrypted_message[16:32], encrypted_message[32:]
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     try:
+        encrypted_message = base64.b64decode(encrypted_message)
+        nonce, tag, ciphertext = encrypted_message[:16], encrypted_message[16:32], encrypted_message[32:]
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
         decrypted_message = cipher.decrypt_and_verify(ciphertext, tag)
-    except ValueError:
-        raise ValueError("Incorrect decryption")
-    return decrypted_message.decode('utf-8')
+        return decrypted_message.decode('utf-8')
+    except (ValueError, KeyError) as e:
+        raise ValueError(f"Incorrect decryption: {e}")
+
+# if __name__ == "__main__":
+#     # Test simple pour vérifier le chiffrement et le déchiffrement
+#     key = generate_aes_key()
+#     message = "This is a secret message"
+#     print(message)
+#     encrypted_message = aes_encrypt(message, key)
+#     print(f"Encrypted: {encrypted_message}")
+#
+#     decrypted_message = aes_decrypt(encrypted_message, key)
+#     print(f"Decrypted: {decrypted_message}")
+#
+#     assert message == decrypted_message, "Le message déchiffré ne correspond pas au message original"
+#     print("Test réussi !")
